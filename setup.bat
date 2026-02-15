c3df0e4da630fee70bc1129d132b4a04dad2cb35
AltWolf <altwolf94@gmail.com>
2026-02-15 18:55:08 +0100

    update setup

--- setup.bat
+++ setup.bat
@@ -5,8 +5,8 @@ REM
 python -m pip install --upgrade pip
 
 REM
-pip install phonenumbers requests discord.py pyngrok flask colorama pillow pyinstaller certifi
-
+pip install phonenumbers requests discord.py flask colorama pillow PyInstaller pyngrok PyQt5 beautifulsoup4 reportlab python-magic-bin pefile lxml urllib3 PyPDF2 pywin32 cryptography psutil
+python -m pip install phonenumbers requests discord.py flask colorama pillow PyInstaller pyngrok PyQt5 beautifulsoup4 reportlab python-magic-bin pefile lxml urllib3 PyPDF2 pywin32 cryptography psutil
 echo.
 echo All packages installed!
 pause
